0d7edcb105e606a178414ad2af91ef292e54b0d5
zhatfiel <zhatfiel@uncc.edu>
2020-11-20 20:43:21 -0500

    Implemented ESLint and used google configuration

--- main.js
+++ main.js
@@ -1,72 +1,76 @@
 const budget = {
-    myBudget: [
-        {
-            title: 'Phone',
-            budget: 35
-        },
-        {
-            title: 'Internet',
-            budget: 45
-        },
-        {
-            title: 'Education',
-            budget: 80
-        },
-        {
-            title: 'Eat out',
-            budget: 25
-        },
-        {
-            title: 'Rent',
-            budget: 275
-        },
-        {
-            title: 'Grocery',
-            budget: 110
-        },
-        {
-            title: 'Pharmacy',
-            budget: 40
-        },
-        {
-            title: 'Donations',
-            budget: 10
-        },
-    ]
+  myBudget: [
+    {
+      title: 'Phone',
+      budget: 35,
+    },
+    {
+      title: 'Internet',
+      budget: 45,
+    },
+    {
+      title: 'Education',
+      budget: 80,
+    },
+    {
+      title: 'Eat out',
+      budget: 25,
+    },
+    {
+      title: 'Rent',
+      budget: 275,
+    },
+    {
+      title: 'Grocery',
+      budget: 110,
+    },
+    {
+      title: 'Pharmacy',
+      budget: 40,
+    },
+    {
+      title: 'Donations',
+      budget: 10,
+    },
+  ],
 };
 
+/**
+ *
+ * @param {budget} b
+ */
 function generateTable(b) {
-    const p = document.getElementById('list');
-    p.innerHTML = '';
-    for (var i = 0; i < b.length; i++) {
-        const div = document.createElement('div');
-        div.innerHTML = `${b[i].title} - ${b[i].budget}`;
-        p.appendChild(div);
-    }
+  const p = document.getElementById('list');
+  p.innerHTML = '';
+  for (let i = 0; i < b.length; i++) {
+    const div = document.createElement('div');
+    div.innerHTML = `${b[i].title} - ${b[i].budget}`;
+    p.appendChild(div);
+  }
 }
 
 generateTable(budget.myBudget);
 
 function aZ() {
-    const newArray = [...budget.myBudget];
-    newArray.sort((a, b) => (a.title > b.title) ? 1 : - 1);
-    generateTable(newArray);
+  const newArray = [...budget.myBudget];
+  newArray.sort((a, b) => (a.title > b.title) ? 1 : - 1);
+  generateTable(newArray);
 }
 
 function zA() {
-    const newArray = [...budget.myBudget];
-    newArray.sort((a, b) => (a.title < b.title) ? 1 : - 1);
-    generateTable(newArray);
+  const newArray = [...budget.myBudget];
+  newArray.sort((a, b) => (a.title < b.title) ? 1 : - 1);
+  generateTable(newArray);
 }
 
 function biggerBudget() {
-    const newArray = [...budget.myBudget];
-    newArray.sort((a, b) => (a.budget < b.budget) ? 1 : - 1);
-    generateTable(newArray);
+  const newArray = [...budget.myBudget];
+  newArray.sort((a, b) => (a.budget < b.budget) ? 1 : - 1);
+  generateTable(newArray);
 }
 
 function smallerBudget() {
-    const newArray = [...budget.myBudget];
-    newArray.sort((a, b) => (a.budget > b.budget) ? 1 : - 1);
-    generateTable(newArray);
+  const newArray = [...budget.myBudget];
+  newArray.sort((a, b) => (a.budget > b.budget) ? 1 : - 1);
+  generateTable(newArray);
 }
